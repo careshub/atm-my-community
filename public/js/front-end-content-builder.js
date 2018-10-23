483212295524caf56d8ebdbb9b4f2787c33ee1a2
David Cavins <david.cavins@gmail.com>
2018-10-23 09:06:10 -0500

    Revert "Scroll back up when the list is collapsed."
    This reverts commit 46759d4b6ac231b88c9006a5649cfe30bdf2487f.

--- public/js/front-end-content-builder.js
+++ public/js/front-end-content-builder.js
@@ -443,11 +443,6 @@
                             $btn.on("click", function (e) {
                                 if (/Less$/.test($btn.html())) {
                                     $btn.html("Show More");
-									
-									// scroll back up so we won't be looking at the bottom of page
-									$('html, body').stop().animate({ 
-										scrollTop: $list.offset().top
-									}, 500);
                                 } else {
                                     $btn.html("Show Less");
                                 }
